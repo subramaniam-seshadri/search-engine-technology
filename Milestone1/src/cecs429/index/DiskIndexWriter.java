0bf037bb09b19ff9f642f8eb87eda0179aefa0b5
Subramaniam Seshadri <subramaniam-seshadri@users.noreply.github.com>
2018-10-18 17:39:27 -0700

    Testing Postings write

--- Milestone1/src/cecs429/index/DiskIndexWriter.java
+++ Milestone1/src/cecs429/index/DiskIndexWriter.java
@@ -1,12 +1,18 @@
 package cecs429.index;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.junit.Test;
 
 public class DiskIndexWriter {
 
@@ -21,19 +27,25 @@ public class DiskIndexWriter {
 		}
 	}
 
+	@Test
 	public void createPostingsFile(Index index, String path, List<String> vocabulary) throws IOException {
 		// Need to be opened and written in binary mode
 		PositionalInvertedIndex pIndex = (PositionalInvertedIndex) index;
 		try {
 			FileOutputStream outputStream = new FileOutputStream(path + "//postings.bin");
 			int currentPosition = 0;
-			for (String term : vocabulary) {
+			 DataOutputStream outStream = new DataOutputStream(new BufferedOutputStream(outputStream));
+			 for(int i =0;i<5000;i++)
+			    outStream.writeInt(121213);
+			
+			  
+			/*for (String term : vocabulary) {
 				List<Posting> postings = pIndex.vocabulary.get(term);
 				DataOutputStream outStream = new DataOutputStream(new BufferedOutputStream(outputStream));
 			    outStream.writeBytes((postings.toString()));
 			    System.out.println(term + " - " + currentPosition);
 			    currentPosition += postings.toString().getBytes().length;
-			}
+			}*/
 			outputStream.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
